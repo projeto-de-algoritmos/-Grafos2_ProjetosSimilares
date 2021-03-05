2ff16b3e93a3127c093fc0c59d1b51c9ccffa4da
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2021-03-05 15:57:07 -0300

    FEAT: render MST

--- projetossimilares/src/utils/constructGraph.js
+++ projetossimilares/src/utils/constructGraph.js
@@ -1,11 +1,22 @@
 import kruskalMST from './kruskal';
+import renderGraph from './renderGraph';
+
+function compareKey(a, b) {
+  if (a.key < b.key) {
+    return -1;
+  }
+  if (a.key > b.key) {
+    return 1;
+  }
+  return 0;
+}
 
 // Funções de criação de grafo
 const adjList = new Map();
 const edgesWeight = new Map();
 
 function addNode(repo) {
-    adjList.set(repo, []);
+  adjList.set(repo, []);
 }
 
 function addEdge(origin, destination) {
@@ -13,40 +24,69 @@ function addEdge(origin, destination) {
   adjList.get(destination).push(origin);
 }
 
-function addEdgeWeight(origin, destination){
+function addEdgeWeight(origin, destination) {
   let sortedName;
-  if(origin<destination){
+  if (origin < destination) {
     sortedName = `${origin}/${destination}`;
   }
-  else{
+  else {
     sortedName = `${destination}/${origin}`;
   }
-  if(edgesWeight.get(sortedName)){
-    edgesWeight.set(sortedName, edgesWeight.get(sortedName)+1);
+  if (edgesWeight.get(sortedName)) {
+    edgesWeight.set(sortedName, edgesWeight.get(sortedName) + 1);
   }
-  else{
+  else {
     edgesWeight.set(sortedName, 1);
   }
 }
 
-function addAllEdges(reposArray){
-  for(let i=0; i< reposArray.length; i++){
-    for(let j=i+1; j<reposArray.length; j++){
+function addAllEdges(reposArray) {
+  for (let i = 0; i < reposArray.length; i++) {
+    for (let j = i + 1; j < reposArray.length; j++) {
       addEdge(reposArray[i], reposArray[j]);
       addEdgeWeight(reposArray[i], reposArray[j]);
     }
   }
 }
 
-function constructGraph({adjListLanguages, adjListRepos}){
+function render(MST, newWeigths) {
+  const graph = Array.from(MST, ([key, value]) => ({ key, value })).sort(compareKey);
+  const edges = Array.from(newWeigths, ([key, value]) => ({ key, value })).sort(compareKey);
+
+  let edgesArray = [];
+  const nodes = graph
+    .filter(repo => repo.key)
+    .map(repo => { return { data: { id: repo.key } } });
+
+  edges
+    .filter(repo => repo.value[0] !== null)
+    .forEach(repo => {
+      let [source, target] = repo.key.split('/');
+      edgesArray = edgesArray.concat(
+        {
+          data: {
+            id: repo.key,
+            source,
+            target
+          },
+          style: {
+            label: repo.value
+          }
+        });
+    })
+
+  const elements = [...nodes, ...edgesArray];
+  renderGraph({ elements });
+}
+
+function constructGraph({ adjListLanguages, adjListRepos }) {
   const repoNames = Array.from(adjListRepos.keys());
   repoNames.forEach(repo => addNode(repo));
 
   const reposInLanguages = Array.from(adjListLanguages.values());
   reposInLanguages.forEach(repos => addAllEdges(repos));
-  const MST = kruskalMST(adjList, edgesWeight);
-  //console.log(MST);
+  const { MST, newWeigths } = kruskalMST(adjList, edgesWeight);
+  render(MST, newWeigths);
 }
-
 
 export default constructGraph;
